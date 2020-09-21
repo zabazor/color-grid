9071807b3d534e82eedade76796b7b324e833c7f
Andrew Wagner <a.j.wagner1212@gmail.com>
2020-09-21 15:08:24 -0400

    -Added the Tray styling -Moved colors to a global color scss -Added a button to generate the 5 tiles ---Still need to apply a deep clone to the tiles to avoid errors - I think

--- src/app/shared/tile-tray/tile-tray.component.ts
+++ src/app/shared/tile-tray/tile-tray.component.ts
@@ -4,6 +4,7 @@ import { SHAPES } from 'src/app/core/data';
 import { ColorGreen, ColorPurple, ColorRed } from 'src/app/core/data/colors';
 import { Tile } from 'src/app/core/classes';
 import { GameEngineService } from 'src/app/core/services';
+import { ɵangular_packages_platform_browser_dynamic_platform_browser_dynamic_a } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'cg-tile-tray',
@@ -11,11 +12,28 @@ import { GameEngineService } from 'src/app/core/services';
   styleUrls: ['./tile-tray.component.scss'],
 })
 export class TileTrayComponent implements OnInit {
-  tile: Tile;
+  tile1: Tile;
+  tile2: Tile;
+  tile3: Tile;
+  tile4: Tile;
+  tile5: Tile;
+
+  tiles: Tile[];
+  // bad starting array to represnet the number of players
+  players = [1, 2, 3, 4];
 
   constructor(private gameEngineService: GameEngineService) {}
 
   ngOnInit(): void {
-    this.tile = this.gameEngineService.drawATile();
+    this.drawTilesPerPlayers();
+  }
+
+  drawTilesPerPlayers() {
+    this.tiles = [];
+
+    let i = 0;
+    for (i; i < this.players.length + 1; i++) {
+      this.tiles.push(this.gameEngineService.drawATile());
+    }
   }
 }
