a2a4702774ab2471a0b4dd4a8ad52b1f7b15da8f
Andrew Wagner <a.j.wagner1212@gmail.com>
2020-09-26 09:03:30 -0400

    -The Player Grid now applies he selected tile's shape and color to the grid on click

--- src/app/shared/player-grid/player-grid.component.ts
+++ src/app/shared/player-grid/player-grid.component.ts
@@ -25,8 +25,46 @@ export class PlayerGridComponent implements OnInit {
   }
 
   placeTile(cell: PlayerGridCell): void {
-    // Overly simplified - will probably need to use the row and cell values to find it in the grid and change others
-    cell.color = this.selectedTile.color;
+    const selectedGridCells = this.getSelectedGridCells(cell);
+
+    for (const selectedGridCell of selectedGridCells) {
+      selectedGridCell.color = this.selectedTile.color;
+    }
+
+    // Clear all grid cells of hover
+
     this.removedSelectedTileEvent.emit();
+  }
+
+  hoverTile(cell: PlayerGridCell): void {
+    const selectedGridCells = this.getSelectedGridCells(cell);
+
+    // we need a temporary color shift somehow... Maybe just update the grid cell to have a hover color
+
+    // Clear all other hovered cells that are not part of the selectedGridCells
+  }
+
+  // Add validation in this so if the player is selecting the edge of the grid, the tile center shifts away from that edge
+  getSelectedGridCells(cell: PlayerGridCell): PlayerGridCell[] {
+    const gridCellsToFill = [];
+    let tileRowOffset = -1;
+
+    for (const tileRow of this.selectedTile.shape.layout.rows) {
+      let tileCellOffset = -1;
+      for (const tileCell of tileRow) {
+        if (tileCell) {
+          const gridRowIndex = cell.rowIndex + tileRowOffset;
+          const gridCellIndex = cell.cellIndex + tileCellOffset;
+          gridCellsToFill.push(this.grid.rows[gridRowIndex][gridCellIndex]);
+          // Maybe instead, have this add a call back function that tells us what to do here
+          // This might be helpful because we want to fill hovered cells and un-fill not hovered cells on hover
+          // and we want to apply color and remove all hover cells on clock
+        }
+        tileCellOffset++;
+      }
+      tileRowOffset++;
+    }
+
+    return gridCellsToFill;
   }
 }
